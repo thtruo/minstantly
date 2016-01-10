77492ef4887061697a6ce33b97d908b3d005b157
thtruo <ttruongchau@gmail.com>
2016-01-09 23:01:38 -0800

    enhanced message window to show each user's avatar and username

--- minstant.js
+++ minstant.js
@@ -9,37 +9,37 @@ if (Meteor.isClient) {
   Router.route('/', function () {
     console.log("rendering root /");
     this.render("navbar", {to:"header"});
-    this.render("lobby_page", {to:"main"});  
+    this.render("lobby_page", {to:"main"});
   });
 
   // specify a route that allows the current user to chat to another users
   Router.route('/chat/:_id', function () {
-    // the user they want to chat to has id equal to 
-    // the id sent in after /chat/... 
+    // the user they want to chat to has id equal to
+    // the id sent in after /chat/...
     var otherUserId = this.params._id;
     // find a chat that has two users that match current user id
     // and the requested user id
     var filter = {$or:[
-                {user1Id:Meteor.userId(), user2Id:otherUserId}, 
+                {user1Id:Meteor.userId(), user2Id:otherUserId},
                 {user2Id:Meteor.userId(), user1Id:otherUserId}
                 ]};
     var chat = Chats.findOne(filter);
     if (!chat){// no chat matching the filter - need to insert a new one
       chatId = Chats.insert({user1Id:Meteor.userId(), user2Id:otherUserId});
     }
-    else {// there is a chat going already - use that. 
+    else {// there is a chat going already - use that.
       chatId = chat._id;
     }
     if (chatId){// looking good, save the id to the session
       Session.set("chatId",chatId);
     }
     this.render("navbar", {to:"header"});
-    this.render("chat_page", {to:"main"});  
+    this.render("chat_page", {to:"main"});
   });
 
   ///
-  // helper functions 
-  /// 
+  // helper functions
+  ///
   Template.available_user_list.helpers({
     users:function(){
       return Meteor.users.find();
@@ -49,7 +49,7 @@ if (Meteor.isClient) {
     getUsername:function(userId){
       user = Meteor.users.findOne({_id:userId});
       return user.profile.username;
-    }, 
+    },
     isMyUser:function(userId){
       if (userId == Meteor.userId()){
         return true;
@@ -65,13 +65,32 @@ if (Meteor.isClient) {
     messages:function(){
       var chat = Chats.findOne({_id:Session.get("chatId")});
       return chat.messages;
-    }, 
+    },
     other_user:function(){
-      return ""
-    }, 
-
+      // We know the current user is Meteor.userId().
+      // The other must be one of user1Id or user2Id that is not Meteor.userId
+      var messages = messages();
+      if (Meteor.userId() == messages.user1Id) {
+        console.log('other_user', messages.user2Id);
+        return messages.user2Id;
+      }
+      console.log('other_user', messages.user1Id);
+      return messages.user1Id;
+    }
   })
- Template.chat_page.events({
+
+  Template.chat_message.helpers({
+    getUsername:function(userId){
+      user = Meteor.users.findOne({_id:userId});
+      return user.profile.username;
+    },
+    getAvatar:function(userId){
+      user = Meteor.users.findOne({_id:userId});
+      return user.profile.avatar;
+    }
+  })
+
+  Template.chat_page.events({
   // this event fires when the user sends a message on the chat page
   'submit .js-send-chat':function(event){
     // stop the form from triggering a page reload
@@ -85,9 +104,9 @@ if (Meteor.isClient) {
         msgs = [];
       }
       // is a good idea to insert data straight from the form
-      // (i.e. the user) into the database?? certainly not. 
+      // (i.e. the user) into the database?? certainly not.
       // push adds the message to the end of the array
-      msgs.push({text: event.target.chat.value});
+      msgs.push({text: event.target.chat.value, user: Meteor.userId()});
       // reset the form
       event.target.chat.value = "";
       // put the messages array onto the chat object
@@ -102,7 +121,7 @@ if (Meteor.isClient) {
 
 // start up script that creates some users for testing
 // users have the username 'user1@test.com' .. 'user8@test.com'
-// and the password test123 
+// and the password test123
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
@@ -114,6 +133,6 @@ if (Meteor.isServer) {
         console.log("creating a user with password 'test123' and username/ email: "+email);
         Meteor.users.insert({profile:{username:username, avatar:avatar}, emails:[{address:email}],services:{ password:{"bcrypt" : "$2a$10$I3erQ084OiyILTv8ybtQ4ON6wusgPbMZ6.P33zzSDei.BbDL.Q4EO"}}});
       }
-    } 
+    }
   });
 }
